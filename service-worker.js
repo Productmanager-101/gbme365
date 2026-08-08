746766c2839f52d5696cf99c6c313f28c0a24792
Productmanager-101 <ococ5332@gmail.com>
2026-08-08 22:11:30 +0900

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,8 +1,11 @@
-
-const CACHE="gbme365-complete-v1";
+const CACHE="gbme365-complete-v2";
 const ASSETS=["./","./index.html","./style.css","./app.js","./content.js","./manifest.json"];
+
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
+self.addEventListener("activate",e=>e.waitUntil(
+  caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))
+));
+
 self.addEventListener("fetch",e=>e.respondWith(
   caches.match(e.request).then(r=>r||fetch(e.request).then(resp=>{
     const copy=resp.clone();
